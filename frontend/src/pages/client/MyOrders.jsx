7097b41f071f2d65be299bdaeeace09b5cf6ffbd
phamhieu100906 <phamhieu100906@gmail.com>
2026-06-25 11:14:44 +0700

    fix: change API_BASE to correct relative url for deployment

--- frontend/src/pages/client/MyOrders.jsx
+++ frontend/src/pages/client/MyOrders.jsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useAuthStore } from '../../context/authStore';
 
-const API_BASE = 'http://localhost:3000/api';
+const API_BASE = '/api';
 
 export default function MyOrders() {
   const navigate = useNavigate();
